b94fc74ddc43b66a2c38b8e2a638479ea35eaf93
RenanFernandoRuiz <renanfernandor908@gmail.com>
2023-08-10 11:07:35 -0300

    ajustes no CSS e JS

--- js/script.js
+++ js/script.js
@@ -3,4 +3,12 @@ var toggleMode = () => {
 
     html.classList.toggle("light");
 
+    const isLight = html.classList.contains("light");
+    const srcValue = isLight ? "../img/avatar-light.jpg" : "../img/avatar.jpg";
+    const altValue = isLight ? "Imagem de perfil com um avatar  na cor azul" : "Imagem de perfil com um avatar na cor vermelha";
+
+    document.querySelector("#profile img").setAttribute("src",srcValue);
+    document.querySelector("#profile img").setAttribute("alt",altValue);
+
+    
 };
